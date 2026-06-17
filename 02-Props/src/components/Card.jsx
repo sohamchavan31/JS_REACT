de09f0cf6e35b4cd7ea1085eb3bb80c62d453506
soham chavan <sohamchavsn03@gmail.com>
2026-06-17 19:22:47 +0530

    props

---
+++ 02-Props/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='parent '>
+      {/* <h1>Props</h1> */}
+      <div className="card">
+        <img src="https://unsplash.com/illustrations/the-wave-and-coconut-summer-time-in-yin-yang-concept-for-badge-patch-emblem-graphic-vector-art-t-shirt-design-B_4WKBVNfUs"/>
+        <h2>Soham Chavan</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum optio recusandae harum corrupti id reprehenderit nihil distinctio quibusdam ratione. Rerum nisi at aspernatur dolor.</p>
+        <button>View Profile </button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
