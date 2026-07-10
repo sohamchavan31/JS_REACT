1296295e4731d4790354534870a8164fa54a8a3f
soham chavan <sohamchavsn03@gmail.com>
2026-07-10 19:30:16 +0530

    Add multer memory storage upload middleware to 04_proj

--- BACKEND/04_proj/src/app.js
+++ BACKEND/04_proj/src/app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const multer = require('multer')
 
 const app = express()
-
 app.get(express.json())
+
+const upload = multer({storage:multer.memoryStorage()})
 
 app.post('/creat-post', async(req, res)=>{
     console.log(req.body);
