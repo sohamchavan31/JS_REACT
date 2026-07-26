c91dc033113448a364600f3ac40090da33db456d
soham <sohamchavsn03@gmail.com>
2026-07-26 18:28:40 +0530

    19

--- 19-advance-routing/src/pages/About.jsx
+++ 19-advance-routing/src/pages/About.jsx
@@ -3,14 +3,16 @@ import { useNavigate } from 'react-router-dom'
 
 const About = () => {
 
-  const navi = useNavigate()
-
-  const btnclicked = ()=>{
-    navi('/')
-  }
+  const navi = useNavigate() 
   return (
     <>
-      <button onClick={btnclicked} className='font-medium bg-emerald-700 px-5 py-2 rounded m-2 cursor-pointer scale'>return to homepage</button>
+      <button onClick={()=>{
+        navi('/')
+      }} className='font-medium bg-emerald-700 px-5 py-2 rounded m-2 cursor-pointer scale'>return to homepage</button>
+      
+      <button onClick={()=>{
+        navi(-1)
+      }} className='font-medium bg-emerald-700 px-5 py-2 rounded m-2 cursor-pointer scale'>Back</button>
       <div className='flex items-center justify-center h-[70vh] text-3xl font-semibold'>About</div>
     </>
   )
