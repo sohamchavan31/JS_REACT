3b1f154a0afe261e8d61108d115bdc4d1da36fe0
soham chavan <sohamchavsn03@gmail.com>
2026-09-04 19:17:08 +0530

    Add authentication system routes and controllers

--- BACKEND/08-authentication_system/src/config/config.js
+++ BACKEND/08-authentication_system/src/config/config.js
@@ -2,8 +2,17 @@ import dotenv from "dotenv"
 
 dotenv.config();  
 
+if(!process.env.MONGO_URI){
+    throw new Error("MONGO_URI is not defined in environment variable")
+}
+
+if(!process.env.JWT_KEY){
+    throw new Error("JWT_KEY is not defined in environment variable")
+}
+
 const config = {
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_KEY: process.env.JWT_KEY
 }
 
 export default config;
